de79b611c9217d71f2c414e592267a1de4b2205d
tirthabig0 <43231335+tirthabig0@users.noreply.github.com>
2021-02-03 13:28:20 +0530

    changes in homepage

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -18,8 +18,8 @@ export default class PostPreview extends React.Component {
 
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
-            <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
-            <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>
+            {/*<h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
+            <p className="w-60-l mb0">{entry.getIn(["data", "blurb", "text"])}</p>*/}
           </div>
         </div>
 
